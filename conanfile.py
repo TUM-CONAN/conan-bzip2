31ec3a0dfc9da4c1493b17255c6345feaa2789ff
NARVIS-UECK\ueck <ulrich.eck@tum.de>
2017-12-29 16:25:54 +0100

    updated to lasote's way of compiling bzip2, changed bintray organization due to quota

--- conanfile.py
+++ conanfile.py
@@ -1,49 +1,59 @@
-from conans import ConanFile, CMake, tools
-import os, shutil
+import os
+import shutil
+
+from conans import CMake, tools
+from conans import ConanFile
+
 
 class Bzip2Conan(ConanFile):
     name = "bzip2"
     version = "1.0.6"
-    description = 'bzip2 is a freely available, patent free (see below), high-quality data compressor.'
-    license = "BSD-style license"
-    url = "https://github.com/ulricheck/conan-bzip2"
+    branch = "master"
+    generators = "cmake"
     settings = "os", "compiler", "arch", "build_type"
-    generators = []
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = "shared=False", "fPIC=True"
+    exports = ["CMakeLists.txt"]
+    url = "https://github.com/ulricheck/conan-bzip2"
+    license = "BSD-style license"
+    description = "bzip2 is a freely available, patent free (see below), high-quality data " \
+                  "compressor. It typically compresses files to within 10% to 15% of the best" \
+                  " available techniques (the PPM family of statistical compressors), whilst " \
+                  "being around twice as fast at compression and six times faster at decompression."
 
-    def source(self):
-        tools.download('http://www.bzip.org/%s/bzip2-%s.tar.gz' % (self.version, self.version), 'bzip2.tar.gz')
-        tools.unzip('bzip2.tar.gz')
-        os.unlink('bzip2.tar.gz')
-        os.rename('bzip2-%s' % self.version, self.name)
-        
-        if self.settings.os == "Windows":
-            tools.replace_in_file('bzip2/makefile.msc', 'CFLAGS= -DWIN32 -MD -Ox -D_FILE_OFFSET_BITS=64 -nologo', '''
-!IF "$(CFG)" == "debug"
-CFG_CFLAGS=-nologo -DWIN32 -D_FILE_OFFSET_BITS=64 -fp:fast -arch:AVX -Zi -Fdlibbz2.pdb -MDd -Od 
-CFG_LDFLAGS=
-!ELSE
-CFG_CFLAGS=-nologo -DWIN32 -D_FILE_OFFSET_BITS=64 -fp:fast -arch:AVX -Zi -Fdlibbz2.pdb -MD -Ox -Oi -Ot -GT -GL
-CFG_LDFLAGS=-LTCG
-!ENDIF''')
-            tools.replace_in_file('bzip2/makefile.msc', '$(CFLAGS)', '$(CFG_CFLAGS)')
-            tools.replace_in_file('bzip2/makefile.msc', 'lib /out:libbz2.lib $(OBJS)', 'lib $(CFG_LDFLAGS) /out:libbz2.lib $(OBJS)')
-            tools.replace_in_file('bzip2/makefile.msc', 'del *.obj', 'del *.obj\n\tdel *.pdb')
+    @property
+    def zip_folder_name(self):
+        return "bzip2-%s" % self.version
 
-    def configure(self):
+    def config(self):
         del self.settings.compiler.libcxx
 
+    def source(self):
+        zip_name = "bzip2-%s.tar.gz" % self.version
+        tools.download("http://www.bzip.org/%s/%s" % (self.version, zip_name), zip_name)
+        tools.check_md5(zip_name, "00b516f4704d4a7cb50a1d97e6e8e15b")
+        tools.unzip(zip_name)
+        os.unlink(zip_name)
+
     def build(self):
-        config = str(self.settings.build_type).lower()
-        with tools.chdir('bzip2'):
-            if self.settings.os == "Windows":
-                self.run('nmake -f makefile.msc CFG=%s' % config)
-            else:
-                self.run('make -f Makefile CFG=%s' % config)
+        shutil.move("CMakeLists.txt", "%s/CMakeLists.txt" % self.zip_folder_name)
+        with tools.chdir(self.zip_folder_name):
+            os.mkdir("_build")
+            with tools.chdir("_build"):
+                cmake = CMake(self)
+                if self.options.fPIC:
+                    cmake.definitions["FPIC"] = "ON"
+                cmake.configure(build_dir=".", source_dir="..")
+                cmake.build(build_dir=".")
 
     def package(self):
-        self.copy("bzlib.h", dst="include", src="bzip2", keep_path=False)
-        self.copy("libbz2.lib", dst="lib", src='bzip2', keep_path=False)
-        self.copy("libbz2.pdb", dst="lib", src='bzip2', keep_path=False)
+        self.copy("*.h", "include", "%s" % self.zip_folder_name, keep_path=False)
+        self.copy("*bzip2", dst="bin", src=self.zip_folder_name, keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", src=self.zip_folder_name, keep_path=False)
+        self.copy(pattern="*.dylib", dst="lib", src=self.zip_folder_name, keep_path=False)
+        self.copy(pattern="*.a", dst="lib", src="%s/_build" % self.zip_folder_name, keep_path=False)
+        self.copy(pattern="*.lib", dst="lib", src="%s/_build" % self.zip_folder_name, keep_path=False)
+        self.copy(pattern="*.dll", dst="bin", src="%s/_build" % self.zip_folder_name, keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["libbz2"]
+        self.cpp_info.libs = ['bz2']
